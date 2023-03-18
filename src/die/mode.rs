7879d4837f453209634cc5de85c50c6082f0abc0
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-03-18 13:22:29 -0500

    add comparison modes

--- src/die/mode.rs
+++ src/die/mode.rs
@@ -1,14 +1,14 @@
 pub enum RollMode {
-    KeepHighest,
-    KeepLowest,
-    DropHighest,
-    DropLowest,
-    Reroll,
-    Exploding,
-    Compounding,
-    Penetrating,
-    CountSuccesses,
-    CountFailures,
+    KeepHighest(i64),
+    KeepLowest(i64),
+    DropHighest(i64),
+    DropLowest(i64),
+    Reroll(ComparisonMode, i64),
+    Exploding(ComparisonMode, i64),
+    Compounding(ComparisonMode, i64),
+    Penetrating(ComparisonMode, i64),
+    CountSuccesses(ComparisonMode, i64),
+    CountFailures(ComparisonMode, i64),
 }
 
 pub enum ComparisonMode {
