9ecb253dbdfe4431303faed96ed728d1af4c18ee
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-03-19 20:05:34 -0500

    end of day

--- src/die/mode.rs
+++ src/die/mode.rs
@@ -1,15 +1,11 @@
 pub enum RollMode {
     Normal,
-    KeepHighest(i64),
-    KeepLowest(i64),
-    DropHighest(i64),
-    DropLowest(i64),
     Reroll(i64),
     Exploding(i64),
     Compounding(i64),
     Penetrating(i64),
-    CountSuccesses(i64),
-    CountFailures(i64),
+    Success(i64),
+    Failure(i64),
 }
 
 pub enum ComparisonMode {
