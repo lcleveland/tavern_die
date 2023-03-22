b96796879f5f0240af628408b3783c6b9db43928
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-03-21 20:12:49 -0500

    add doc

--- src/die/roll_result.rs
+++ src/die/roll_result.rs
@@ -1,9 +1,11 @@
 /// Represents the results returned when a die is rolled
 pub struct RollResult {
+    /// Holds all the rolls of a die, based on mode
     pub results: Vec<i64>,
 }
 
 impl RollResult {
+    /// Creates a new roll result
     pub fn new() -> RollResult {
         RollResult {
             results: Vec::new(),
