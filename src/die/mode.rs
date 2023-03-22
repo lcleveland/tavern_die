17dc7a7004f1c57785f597935012fd5abea83860
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-03-21 20:10:39 -0500

    add documentation

--- src/die/mode.rs
+++ src/die/mode.rs
@@ -1,15 +1,34 @@
 pub enum RollMode {
+    /// This is a straight die roll, no comparison modes are used
     Normal,
+
+    /// Reroll the die if comparison is met. Discards old die roll
     Reroll,
+
+    /// If comparison is met adds die roll to results and rolls again
     Exploding,
+
+    /// If comparison is met adds all die rolls that match together in results
     Compounding,
+
+    /// If comparison is met adds die roll to results and rolls again, subtracting one from the
+    /// penetrated roll
     Penetrating,
+
+    /// If the comparison is met adds the die roll to results to show as a success
     Success,
+
+    /// If the comparison is NOT met adds the die roll to results to show as a failure
     Failure,
 }
 
 pub enum ComparisonMode {
+    /// Checks that the die roll is greater than the provided number
     GreaterThan(i64),
+
+    /// Checks that the die roll is less than the provided number
     LessThan(i64),
+
+    /// Checks that the die roll is equal to the provided number
     Equal(i64),
 }
